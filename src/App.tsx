dd3f6fce3bf9dfc9f06b491fd20103326b2b7274
Alexksxx <bestuzhev.aleksei.m@gmail.com>
2023-01-25 21:32:15 +0400

    upd: samurai way bm start

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Hello, samurai! Let's go!</h1>
+            <h4>It's just the beginning</h4>
+        </div>
+    );
 }
 
 export default App;
