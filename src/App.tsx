ffc63f4c0c4f99e3c7db6ad9f204d7f576731158
Alexksxx <bestuzhev.aleksei.m@gmail.com>
2023-01-25 21:38:28 +0400

    upd: samurai way bm add some block such as different file component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import SomeBlock from "./SomeBlock";
 
-function App() {
+const App = () => {
     return (
         <div className="App">
             <h1>Hello, samurai! Let's go!</h1>
             <h4>It's just the beginning</h4>
+            <SomeBlock />
         </div>
     );
 }
---
+++ src/SomeBlock.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SomeBlock = () => {
+    return (
+        <div>
+            <h5>Block</h5>
+        </div>
+    );
+}
+
+export default SomeBlock;
