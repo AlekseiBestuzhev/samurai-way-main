a279d29697f5ed176221a2e3542a6d646a956b75
Alexksxx <bestuzhev.aleksei.m@gmail.com>
2023-01-25 22:22:03 +0400

    upd: samurai way bm create triangle component

--- src/SomeBlock.tsx
+++ src/SomeBlock.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './SomeBlock.css'
+import Triangle from "./Triangle";
 
 const SomeBlock = () => {
     return (
         <div>
             <h5 className='SomeBlock-title'>Block</h5>
+            <Triangle />
         </div>
     );
 }
---
+++ src/Triangle.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Triangle.css'
+
+const Triangle = () => {
+    return (
+        <div className='Triangle-body'></div>
+    );
+}
+
+export default Triangle;
