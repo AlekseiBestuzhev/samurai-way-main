7a63a66abc9d03be39541269096f5466a80281ed
Alexksxx <bestuzhev.aleksei.m@gmail.com>
2023-01-27 00:34:03 +0400

    upd: samurai way sn create blocks

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,12 @@ import './App.css';
 
 const App = () => {
     return (
-        <div className="App">
-
+        <div className="app-wrapper">
+            <header className={'header'}>
+                <img src={'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/NASA_Worm_logo.svg/1200px-NASA_Worm_logo.svg.png'} alt={'Logo'}/>
+            </header>
+            <aside className={'sidebar'}>Menu</aside>
+            <main className={'content'}>Main</main>
         </div>
     );
 }
