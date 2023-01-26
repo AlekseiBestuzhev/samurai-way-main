947ddd6ee46b002de4007281b239528fe82a3114
Alexksxx <bestuzhev.aleksei.m@gmail.com>
2023-01-27 00:07:38 +0400

    upd: samurai way sn start

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
 import React from 'react';
 import './App.css';
-import SomeBlock from "./SomeBlock";
-import Line from "./Line";
 
 const App = () => {
     return (
         <div className="App">
-            <div className='App-title'>
-                <h1>Hello, samurai! Let's go!</h1>
-                <h4>It's just the beginning</h4>
-            </div>
-            <SomeBlock/>
-            <Line/>
-            <p>Lorem ipsum dolar sit amet</p>
-            <Line/>
-            <SomeBlock/>
+
         </div>
     );
 }
