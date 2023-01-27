af0afd8cd4e74fea48d18be3a3d3838c9b5db466
Alexksxx <bestuzhev.aleksei.m@gmail.com>
2023-01-27 17:56:35 +0400

    upd: samurai way sn 11 add photo

--- src/App.tsx
+++ src/App.tsx
@@ -31,8 +31,16 @@ const App = () => {
                 </nav>
             </aside>
             <main className={'content'}>
-                <img className={'page-label'} src={'https://wallup.net/wp-content/uploads/2016/01/257350-galaxy-NASA-space-Milky_Way.jpg'} alt={'Stars'}/>
-                <section>About person</section>
+                <img className={'page-label'}
+                     src={'https://wallup.net/wp-content/uploads/2016/01/257350-galaxy-NASA-space-Milky_Way.jpg'}
+                     alt={'Stars'}/>
+                <section className={'person-block'}>
+                    <img className={'avatar'} src={'https://masyamba.ru/%D0%BA%D0%BE%D1%81%D0%BC%D0%BE%D0%BD%D0%B0%D0%B2%D1%82-%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B8/18-%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B8-%D0%BF%D1%80%D0%BE-%D0%BA%D0%BE%D1%81%D0%BC%D0%BE%D0%BD%D0%B0%D0%B2%D1%82%D0%BE%D0%B2.jpg'} alt={'Avatar'}/>
+                    <div className={'about-text-block'}>
+                        <h2>Alex Nealex</h2>
+                        <p>Something about me</p>
+                    </div>
+                </section>
                 <div>My posts</div>
                 <div>New post</div>
                 <div>Post 1</div>
