275d537b99c29755afb76d5864ff92e5cf60b4f4
Alexksxx <bestuzhev.aleksei.m@gmail.com>
2023-01-28 23:01:10 +0400

    upd: samurai way sn 12 add style profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -10,7 +10,7 @@ const Profile = () => {
                 <img className={'avatar'} src={'https://masyamba.ru/%D0%BA%D0%BE%D1%81%D0%BC%D0%BE%D0%BD%D0%B0%D0%B2%D1%82-%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B8/18-%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B8-%D0%BF%D1%80%D0%BE-%D0%BA%D0%BE%D1%81%D0%BC%D0%BE%D0%BD%D0%B0%D0%B2%D1%82%D0%BE%D0%B2.jpg'} alt={'Avatar'}/>
                 <div className={'about-text-block'}>
                     <h2 className={'person-name'}>Alex Nealex</h2>
-                    <p>Begginer frontend developer</p>
+                    <p>beginner frontend developer</p>
                 </div>
             </section>
             <div>My posts</div>
