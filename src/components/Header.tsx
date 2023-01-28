1ce5d89ffb2aab30cfe84e9a8ef82bf9af169ff3
Alexksxx <bestuzhev.aleksei.m@gmail.com>
2023-01-28 23:33:03 +0400

    upd: samurai way sn 12 add component login button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import LogInButton from "./LogInButton";
 
 const Header = () => {
     return (
         <header className={'header'}>
             <img
-                src={'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/NASA_Worm_logo.svg/1200px-NASA_Worm_logo.svg.png'}
-                alt={'Logo'}/>
+                src={'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/NASA_Worm_logo.svg/1200px-NASA_Worm_logo.svg.png'} alt={'Logo'}/>
+            <LogInButton/>
         </header>
     )
 }
---
+++ src/components/LogInButton.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const LogInButton = () => {
+    return (
+        <a href={'#'} className={'login-button'}>Log In</a>
+    )
+}
+
+export default LogInButton;
