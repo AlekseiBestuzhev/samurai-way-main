5fb8af3bc97aade515c1de5bc49b620ef99c00d8
Aleksei <bestuzhev.aleksei.m@gmail.com>
2023-02-04 01:40:53 +0400

    upd: les_14 create module and move sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,25 +1,25 @@
 import React from "react";
-import './Sidebar.css';
+import cls from './Sidebar.module.css';
 
 const Sidebar = () => {
 	return (
-		<aside className={'sidebar'}>
+		<aside className={cls.sidebar}>
 			<nav>
 				<ul>
 					<li>
-						<a className={'link'} href={'1#'}>Profile</a>
+						<a className={cls.link} href={'1#'}>Profile</a>
 					</li>
 					<li>
-						<a className={'link'} href={'1#'}>Messages</a>
+						<a className={cls.link} href={'1#'}>Messages</a>
 					</li>
 					<li>
-						<a className={'link'} href={'1#'}>News</a>
+						<a className={cls.link} href={'1#'}>News</a>
 					</li>
 					<li>
-						<a className={'link'} href={'1#'}>Music</a>
+						<a className={cls.link} href={'1#'}>Music</a>
 					</li>
 					<li>
-						<a className={'link'} href={'1#'}>Settings</a>
+						<a className={cls.link} href={'1#'}>Settings</a>
 					</li>
 				</ul>
 			</nav>
