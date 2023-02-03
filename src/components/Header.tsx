ef62697c6cdb8c4510de289082c4c97a6c85844a
Aleksei <bestuzhev.aleksei.m@gmail.com>
2023-02-04 00:58:14 +0400

    upd: les_13 separate styles for header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,15 @@
 import React from "react";
+import './Header.css';
 import LogInButton from "./LogInButton";
 
 const Header = () => {
-    return (
-        <header className={'header'}>
-            <img
-                src={'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/NASA_Worm_logo.svg/1200px-NASA_Worm_logo.svg.png'} alt={'Logo'}/>
-            <LogInButton/>
-        </header>
-    )
+	return (
+		<header className={'header'}>
+			<img
+				src={'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/NASA_Worm_logo.svg/1200px-NASA_Worm_logo.svg.png'} alt={'Logo'} />
+			<LogInButton />
+		</header>
+	)
 }
 
 export default Header;
