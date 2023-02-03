8aa129e09a14449962757531c7584bd4e3dfcd76
Aleksei <bestuzhev.aleksei.m@gmail.com>
2023-02-04 01:23:50 +0400

    upd: les_13 create css module for posts block

--- src/components/PostsBlock.tsx
+++ src/components/PostsBlock.tsx
@@ -1,14 +1,15 @@
 import React from "react";
+import cls from "./PostsBlock.module.css";
 
 const PostsBlock = () => {
-    return (
-        <div className={'container'}>
-            <div>My posts</div>
-            <div>New post</div>
-            <div>Post 1</div>
-            <div>Post 2</div>
-        </div>
-    )
+	return (
+		<div className={'container'}>
+			<div className={cls.title}>My posts</div>
+			<div>New post</div>
+			<div>Post 1</div>
+			<div>Post 2</div>
+		</div>
+	)
 }
 
 export default PostsBlock;
