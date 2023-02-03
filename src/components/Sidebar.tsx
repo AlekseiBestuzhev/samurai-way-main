15abe74e642902c253c044a905ad08647e642e92
Aleksei <bestuzhev.aleksei.m@gmail.com>
2023-02-04 01:08:46 +0400

    upd: les_13 separate styles for sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,29 +1,30 @@
 import React from "react";
+import './Sidebar.css';
 
 const Sidebar = () => {
-    return (
-        <aside className={'sidebar'}>
-            <nav>
-                <ul>
-                    <li>
-                        <a className={'link'} href={'#'}>Profile</a>
-                    </li>
-                    <li>
-                        <a className={'link'} href={'#'}>Messages</a>
-                    </li>
-                    <li>
-                        <a className={'link'} href={'#'}>News</a>
-                    </li>
-                    <li>
-                        <a className={'link'} href={'#'}>Music</a>
-                    </li>
-                    <li>
-                        <a className={'link'} href={'#'}>Settings</a>
-                    </li>
-                </ul>
-            </nav>
-        </aside>
-    )
+	return (
+		<aside className={'sidebar'}>
+			<nav>
+				<ul>
+					<li>
+						<a className={'link'} href={'1#'}>Profile</a>
+					</li>
+					<li>
+						<a className={'link'} href={'1#'}>Messages</a>
+					</li>
+					<li>
+						<a className={'link'} href={'1#'}>News</a>
+					</li>
+					<li>
+						<a className={'link'} href={'1#'}>Music</a>
+					</li>
+					<li>
+						<a className={'link'} href={'1#'}>Settings</a>
+					</li>
+				</ul>
+			</nav>
+		</aside>
+	)
 }
 
 export default Sidebar;
