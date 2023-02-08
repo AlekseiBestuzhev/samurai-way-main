dbee4918aa4ddd7d67cf537e1709afbb172decae
Aleksei <bestuzhev.aleksei.m@gmail.com>
2023-02-08 23:46:13 +0400

    upd: les_21 layout messages

--- src/components/Messages/Messages.tsx
+++ src/components/Messages/Messages.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import cls from './Messages.module.css'
 
 export const Messages = () => {
 
 	return (
 		<div className='container'>
-			Messages
+			<h2>Dialogs</h2>
+			<div className={cls.dialogs}>
+				<div className={cls.dialogItems}>
+					<div className={cls.dialog}>Ilysha</div>
+					<div className={cls.dialog}>Dimasik</div>
+					<div className={cls.dialog}>Rymsky</div>
+					<div className={cls.dialog}>Korepan</div>
+					<div className={cls.dialog}>David</div>
+					<div className={cls.dialog}>Blinchik</div>
+				</div>
+				<div className={cls.dialogContent}>
+					<div>Fucking</div>
+					<div>dungeon</div>
+					<div>master</div>
+				</div>
+			</div >
 		</div>
 	);
 }
