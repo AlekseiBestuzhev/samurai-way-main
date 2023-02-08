f50a78099100e4b14f0e1996aefe8d240d886c4d
Aleksei <bestuzhev.aleksei.m@gmail.com>
2023-02-08 19:27:31 +0400

    upd: les_20 refactor styles for buttons

--- src/components/Profile/PostsBlock/NewPost/NewPost.tsx
+++ src/components/Profile/PostsBlock/NewPost/NewPost.tsx
@@ -7,8 +7,8 @@ export const NewPost = () => {
 		<div className={cls.block}>
 			<textarea className={cls.textarea} name="text" id="1" placeholder='Write text for a new post here...'></textarea>
 			<div className={cls.buttons}>
-				<button className={cls.button}>Attach</button>
-				<button className={cls.button}>Share</button>
+				<button className={cls.newPostButton}>Attach</button>
+				<button className={cls.newPostButton}>Share</button>
 			</div>
 		</div>
 	);
