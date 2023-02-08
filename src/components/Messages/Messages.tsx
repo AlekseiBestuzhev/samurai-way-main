3f08f42ee03a674dbe6b28071d3deba42241e989
Aleksei <bestuzhev.aleksei.m@gmail.com>
2023-02-09 00:41:03 +0400

    upd: les_22 add navlinks to dialogs

--- src/components/Messages/Messages.tsx
+++ src/components/Messages/Messages.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import cls from './Messages.module.css'
 
 export const Messages = () => {
 
 	return (
-		<div className='container additionalContainer'>
+		<div className='additionalContainer'>
 			<h2 className={cls.title}>Dialogs</h2>
 			<div className={cls.dialogs}>
 				<div className={cls.dialogItems}>
-					<div className={cls.dialog}>Ilysha</div>
-					<div className={cls.dialog}>Dimasik</div>
-					<div className={cls.dialog}>Rymsky</div>
-					<div className={cls.dialog}>Korepan</div>
-					<div className={cls.dialog}>David</div>
-					<div className={cls.dialog}>Blinchik</div>
+					<NavLink
+						to='/dialogs/Ilysha'
+						className={cls.dialog}>Ilysha</NavLink>
+					<NavLink
+						to='/dialogs/Dimasik'
+						className={cls.dialog}>Dimasik</NavLink>
+					<NavLink
+						to='/dialogs/Rymsky'
+						className={cls.dialog}>Rymsky</NavLink>
+					<NavLink
+						to='/dialogs/Korepan'
+						className={cls.dialog}>Korepan</NavLink>
+					<NavLink
+						to='/dialogs/David'
+						className={cls.dialog}>David</NavLink>
+					<NavLink
+						to='/dialogs/Blinchik'
+						className={cls.dialog}>Blinchik</NavLink>
 				</div>
 				<div className={cls.dialogContent}>
 					<div>Fucking</div>
