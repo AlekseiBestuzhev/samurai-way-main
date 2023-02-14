898d692e138a8fb7a25a102a2745a0c2d1636017
Aleksei <bestuzhev.aleksei.m@gmail.com>
2023-02-14 22:53:51 +0400

    upd: les_25 map posts

--- src/components/Profile/PostsBlock/PostsBlock.tsx
+++ src/components/Profile/PostsBlock/PostsBlock.tsx
@@ -5,17 +5,21 @@ import cls from "./PostsBlock.module.css";
 
 const PostsBlock = () => {
 	const postsData = [
-		{id: 1, name:'Tony Stark', text: 'How are you?', likes: 6},
-		{id: 2, name:'Tony Stark', text: 'You\'re already here?', likes: 9}
+		{ id: 1, name: 'Tony Stark', title: 'How are you?', likes: 6 },
+		{ id: 2, name: 'Tony Stark', title: 'You\'re already here?', likes: 9 }
 	];
+
+	const postsList: JSX.Element | JSX.Element[] =
+		postsData.length
+			? postsData.map(el => <Post id={el.id} name={el.name} title={el.title} likes={el.likes} />)
+			: <div className={cls.emptyList}>Your wall is empty...</div>
 
 	return (
 		<div className={'container'}>
 			<NewPost />
 			<hr />
 			<div className={cls.title}>My wall</div>
-			<Post id={1} name='Tony Stark' title="How are you?" likes={6} />
-			<Post id={2} name='Tony Stark' title="You're already here?"  likes={9}/>
+			{postsList}
 		</div>
 	)
 }
