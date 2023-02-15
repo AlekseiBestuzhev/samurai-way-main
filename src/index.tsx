e57ab05a3f86e33c32d482cbf277271cddf19847
Aleksei <bestuzhev.aleksei.m@gmail.com>
2023-02-15 17:49:14 +0400

    upd:  les_27 move dialogs data to index

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { DialogType } from './components/Messages/Dialog/Dialog';
+
+const dialogsData: DialogType[] = [
+	{ id: 1, name: 'Ilysha' },
+	{ id: 2, name: 'Dimasik' },
+	{ id: 3, name: 'Rymsky' },
+	{ id: 4, name: 'Korepan' },
+	{ id: 5, name: 'David' },
+	{ id: 6, name: 'Blinchik' }
+];
 
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+	<App dialogsData={dialogsData} />,
+	document.getElementById('root')
 );
